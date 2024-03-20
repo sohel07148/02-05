baf467ac96686ac4a1579dc8862c35e698d4dda8
sohel07148 <sohelshaikh07148@gmail.com>
2024-03-20 11:37:59 +0530

    done

---
+++ script.js
@@ -0,0 +1,13 @@
+// console.log("script running....")
+// document.querySelector('.arrow').style.display = 'none';
+// document.querySelector('.hamburger').addEventListener("click", ()=>{
+//     document.querySelector('.sidebar').classList.toggle('sidebarGo');
+// })
+
+// console.log("script running....")
+document.querySelector('.arrow').style.display = 'none';
+document.querySelector('.hamburgr').addEventListener("click", ()=>{
+    console.log("clicked")
+    document.querySelector('.sidebar').classList.toggle('.sidebarGo');
+})
+
